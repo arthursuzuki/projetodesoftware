7426c95d99cc799a1b594848f805eb4c685d6e7c
brandonhunt00 <brandondohunt@hotmail.com>
2025-04-21 14:05:38 -0300

    Finaliza automação dos 10 cenários BDD do Kanban (KAN-1 a KAN-10)

---
+++ src/main/java/com/projetoso/projeto/ProjetosoApplication.java
@@ -0,0 +1,13 @@
+package com.projetoso.projeto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetosoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoso/projeto/ProjetosoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoso.projeto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
